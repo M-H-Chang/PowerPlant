86a06218f28ee7c4a14f7483d1033bac22466b9d
Min Chang <minchangmhc@gmail.com>
2021-06-22 18:17:07 -0400

    ♻️ cleaned code for reusability.
    Co-authored-by: Thomas Friedrichs <friedrichs.tommy@gmail.com>

--- src/Plant.js
+++ src/Plant.js
@@ -1,25 +1,15 @@
-export {hydrate, sunlight, nutrients, genericPlant, increaseState};
+export {stateControl, blueFood, feed, hydrate, superhydrate, storeState, increaseState};
 
-const hydrate = (plant) => {
-  return {
-    ...plant,
-    drinkWater: (plant.drinkWater || 0) + 1
+const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
   };
 };
 
-const sunlight = (plant) => {
-  return {
-    ...plant,
-    absorb: (plant.absorb || 0) + 1 
-  };
-};
-
-const nutrients = (plant) => {
-  return {
-    ...plant,
-    intake: (plant.intake || 0) + 1
-  };
-};
+const stateControl = storeState(); //holds inner function
 
 const increaseState = (property) => {
   return (value) => {
@@ -30,17 +20,51 @@ const increaseState = (property) => {
   };
 };
 
-const feed = increaseState("intake");
-const drink = increaseState("drinkWater");
-const giveLight = increaseState("absorb");
+// const genericPlant = (name) => {
+//   let state = {
+//     name
+//   };
+//   return {...state, ...hydrate(state), ...sunlight(state), ...nutrients(state)};
+// };
+
+const feed = increaseState("intake")(1);
+const blueFood = increaseState("intake")(5);
+
+const hydrate = increaseState("drinkWater")(1);
+const superhydrate = increaseState("drinkWater")(5);
+
+const fedPlant = stateControl(blueFood);
+console.log(fedPlant);
 
 
 
-const genericPlant = (name) => {
-  let state = {
-    name
-  };
-  return {...state, ...hydrate(state), ...sunlight(state), ...nutrients(state)};
-};
+// ***The code below was dried up by the function above "increaseState"***
+// const hydrate = (plant) => {
+//   return {
+//     ...plant,
+//     drinkWater: (plant.drinkWater || 0) + 1
+//   };
+// };
 
-console.log(genericPlant);
+// const sunlight = (plant) => {
+//   return {
+//     ...plant,
+//     absorb: (plant.absorb || 0) + 1 
+//   };
+// };
+
+// const nutrients = (plant) => {
+//   return {
+//     ...plant,
+//     intake: (plant.intake || 0) + 1
+//   };
+// };
+
+
+//const feed = increaseState("intake");
+// const drink = increaseState("drinkWater");
+// const giveLight = increaseState("absorb");
+
+
+
+
